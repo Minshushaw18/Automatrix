58bd3c1da6506a8132d0efa2a39af4786fadd3e1
Minshushaw18 <guptaminshu85@gmail.com>
2024-10-21 21:32:13 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ import { BillingProvider } from '@/providers/billing-provider'
 const font = DM_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Fuzzie.',
-  description: 'Automate Your Work With Fuzzie.',
+  title: 'Automatrix.',
+  description: 'Automate Your Work With Automatrix.',
 }
 
 export default function RootLayout({
--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,66 +1,105 @@
-'use client'
+"use client"
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import React from 'react'
+import React from "react";
+import {
+  motion,
+  useTransform,
+  useMotionValue,
+  useSpring,
+} from "framer-motion";
+
+import { Separator } from "@/components/ui/separator"
+
+
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from '@/components/ui/tooltip'
+} from "@/components/ui/tooltip"
 import { menuOptions } from '@/lib/constant'
 import clsx from 'clsx'
-import { Separator } from '@/components/ui/separator'
-import { Database, GitBranch, LucideMousePointerClick } from 'lucide-react'
-import { ModeToggle } from '../global/mode-toggle'
+import { Database, GitBranch, LucideMousePointerClick } from 'lucide-react';
+import { ModeToggle } from '../global/mode-toggle';
+
 
 type Props = {}
 
-const MenuOptions = (props: Props) => {
+const Sidebar = (props: Props) => {
+
+  const springConfig = { stiffness: 100, damping: 5 };
+  const x = useMotionValue(0); // going to set this value on mouse move
+  // rotate the tooltip
+  const rotate = useSpring(
+    useTransform(x, [-100, 100], [-45, 45]),
+    springConfig
+  );
+  // translate the tooltip
+  const translateX = useSpring(
+    useTransform(x, [-100, 100], [-50, 50]),
+    springConfig
+  );
+
   const pathName = usePathname()
 
   return (
-    <nav className=" dark:bg-black h-screen overflow-scroll  justify-between flex items-center flex-col  gap-10 py-6 px-2">
-      <div className="flex items-center justify-center flex-col gap-8">
-        <Link
-          className="flex font-bold flex-row "
-          href="/"
-        >
-          fuzzie.
+    <nav className='dark:bg-black h-screen overflow-scroll justify-between flex items-center flex-col gap-10 py-6 px-2'>
+      <div className='flex items-center justify-centre flex-col gap-8'>
+        <Link className='flex font-bold flex-row' href='/'>
+          Automatrix
         </Link>
         <TooltipProvider>
-          {menuOptions.map((menuItem) => (
-            <ul key={menuItem.name}>
-              <Tooltip delayDuration={0}>
-                <TooltipTrigger>
-                  <li>
-                    <Link
-                      href={menuItem.href}
-                      className={clsx(
-                        'group h-8 w-8 flex items-center justify-center  scale-[1.5] rounded-lg p-[3px]  cursor-pointer',
-                        {
-                          'dark:bg-[#2F006B] bg-[#EEE0FF] ':
-                            pathName === menuItem.href,
-                        }
-                      )}
+          {
+            menuOptions.map((menuItem) => (
+              <ul key={menuItem.name}>
+                <Tooltip delayDuration={0}>
+                  <TooltipTrigger>
+                    <li>
+                      <Link href={menuItem.href}
+                        className={clsx(
+                          'group h-8 flex items-center justify-center scale-[1.5] rounded-lg p-[3px] cursor-pointer',
+                          {
+                            "dark:bg-[#2F006B] bg-[#EEE0FF]": pathName === menuItem.href,
+                          }
+                        )}
+                      >
+                        <menuItem.Component selected={pathName === menuItem.href} />
+                      </Link>
+                    </li>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <motion.div
+                      initial={{ opacity: 0, y: 20, scale: 0.6 }}
+                      animate={{
+                        opacity: 1,
+                        y: 0,
+                        scale: 1,
+                        transition: {
+                          type: "spring",
+                          stiffness: 260,
+                          damping: 10,
+                        },
+                      }}
+                      exit={{ opacity: 0, y: 20, scale: 0.6 }}
+                      style={{
+                        translateX: translateX,
+                        rotate: rotate,
+                        whiteSpace: "nowrap",
+                      }}
                     >
-                      <menuItem.Component
-                        selected={pathName === menuItem.href}
-                      />
-                    </Link>
-                  </li>
-                </TooltipTrigger>
-                <TooltipContent
-                  side="right"
-                  className="bg-black/10 backdrop-blur-xl"
-                >
-                  <p>{menuItem.name}</p>
-                </TooltipContent>
-              </Tooltip>
-            </ul>
-          ))}
+                      <div className="font-bold text-white relative z-30 text-base">
+                        {menuItem.name}
+                      </div>
+                    </motion.div>
+                  </TooltipContent>
+                  <Separator />
+                </Tooltip>
+              </ul>
+            ))
+          }
         </TooltipProvider>
-        <Separator />
+
         <div className="flex items-center flex-col gap-9 dark:bg-[#353346]/30 py-4 px-2 rounded-full h-56 overflow-scroll border-[1px]">
           <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
             <LucideMousePointerClick
@@ -90,12 +129,12 @@ const MenuOptions = (props: Props) => {
             />
           </div>
         </div>
-      </div>
-      <div className="flex items-center justify-center flex-col gap-8">
-        <ModeToggle />
-      </div>
-    </nav>
+    </div>
+    <div className='flex item-centre justify-center flex-col gap-8'>
+       <ModeToggle/>
+    </div>
+    </nav >
   )
 }
 
-export default MenuOptions
+export default Sidebar
